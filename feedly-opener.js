16a37937b6678eb42ef3047b48be08024eea196b
Jonathan Sharman <jonathan.sharman@niche.com>
2025-09-29 09:43:14 -0400

    Wait for Feedly elements to appear before clicking them

--- feedly-opener.js
+++ feedly-opener.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Feedly Opener
 // @namespace    https://www.jonathansharman.com/
-// @version      0.7.1
+// @version      0.7.2
 // @description  On enter press, opens the first feed item and hides its card.
 // @author       Jonathan Sharman
 // @match        https://feedly.com/i/collection/content/user/*
@@ -17,11 +17,33 @@ document.addEventListener("keyup", function (event) {
         const link = entry.querySelector("a");
         window.open(link.href, "_blank");
         link.click();
-        setTimeout(function () {
-            document.querySelector('.SidePeekCloseTab div').click();
-            setTimeout(function () {
-                document.querySelector('.EntryRemoveButton').click();
+        waitForElement('.SidePeekCloseTab div').then(closeButton => {
+            closeButton.click();
+            waitForElement('.EntryRemoveButton').then(removeButton => {
+                removeButton.click();
             });
         });
     }
 });
+
+// https://stackoverflow.com/a/61511955
+function waitForElement(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
